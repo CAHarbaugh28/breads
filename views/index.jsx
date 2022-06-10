b1b13e2988224505fda3da69febd350065c00f33
Carter Harbaugh <carter.harbaugh@gmail.com>
2022-06-09 21:16:31 -0500

    Adding Bootstrap grid to index page

--- views/index.jsx
+++ views/index.jsx
@@ -9,19 +9,24 @@ function Index ({breads, title})  {
         {/* This is a JSX comment. */}
         <ul>
         {
-        breads.map((bread, index)=> {
-             return (
-                <li key={index}>
-                  <a href={`/breads/&{index}`}>
-                    {bread.name}
-                  </a> 
-                </li>
-             )
-         })
+          breads.map((bread, index)=> {
+            return (
+              <li key={index}>
+                <a href={`/breads/${index}`}>
+                  {bread.name}
+                </a>
+              </li>
+            )
+          })
         }
         </ul>
+        <div className="newButton">
+          <a href="/breads/new"><button>Add a new bread</button></a>
+        </div>
       </Default>            
     )
 }
+
+
 
 module.exports = Index
