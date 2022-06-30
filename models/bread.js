e3c9f5191582a28f0318c632e9355fb4dccf1bb2
Carter Harbaugh <carter.harbaugh@gmail.com>
2022-06-30 16:45:51 -0500

    Able to edit breads

--- models/bread.js
+++ models/bread.js
@@ -7,7 +7,7 @@ const { Schema } = mongoose
 const breadSchema = new Schema({
     name: { type: String, required: true },
     hasGluten: Boolean,
-    image: { type: String, default: 'http://placehold.it/500x500.png' }
+    image: { type: String, default: 'https://static01.nyt.com/images/2013/03/03/magazine/03wmt1/03wmt1-articleLarge-v2.jpg?year=2013&h=453&w=600&s=a17b1fd9a5c5b0e8ffac0039cee9ef7a2f0eb929e810e3c1968d0b6a4a4997b4&k=ZQJBKqZ0VN&tw=1' }
 })
 
 // model and export 
